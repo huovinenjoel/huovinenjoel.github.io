2eecedb00b49485b8caefaf915d4c9b9e3b478ed
Joel Huovinen <joelhuovine@gmail.com>
2023-06-29 13:08:36 +0300

    Added shadcn UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,171 @@
-import Image from 'next/image'
+import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="flex min-h-screen flex-col items-center relative">
+      <div className="relative w-auto h-auto">
+        {/* Otsikot */}
+
+        <h1
+          className="font-extrabold
+                      text-center
+                      text-8xl
+                      pt-52 
+                      bg-gradient-to-t from-slate-950 to-slate-600 
+                      text-transparent 
+                      bg-clip-text 
+                      drop-shadow-2xl"
+        >
+          JOLLEN SIVUSTO
+        </h1>
+        <h2
+          className="font-bold
+                      text-center 
+                      text-4xl 
+                      bg-gradient-to-t from-slate-800 to-slate-500
+                      text-transparent
+                      bg-clip-text 
+                      drop-shadow-2xl"
+        >
+          Sivusto kasautuu vaihe vaiheelta
+        </h2>
+      </div>
+
+      {/* blobs start */}
+
+      <div
+        className="rounded-full 
+                      w-96 h-96 
+                      bg-amber-300 
+                      mix-blend-multiply 
+                      opacity-80 blur-[150px] 
+                      absolute -z-10 
+                      animate-blob"
+      ></div>
+      <div
+        className="rounded-full 
+                      w-96 h-96 
+                      bg-sky-300 
+                      mix-blend-multiply 
+                      opacity-80 blur-[150px] 
+                      absolute -z-10 
+                      right-64 top-72 
+                      animate-blob 
+                      animation-delay-2000"
+      ></div>
+      <div
+        className="rounded-full 
+                      w-96 h-96 
+                      bg-teal-300 
+                      mix-blend-multiply 
+                      opacity-80 blur-[150px] 
+                      absolute -z-10
+                      left-56 top-64 
+                      animate-blob
+                      animation-delay-4000"
+      ></div>
+
+      {/* blobs end */}
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <a className="group mt-48 w-64 h-64 relative ">
+            <img
+              src="/herrahiirijpeg.jpg"
+              alt="Tietoa minusta"
+              className="
+                    rounded-full
+                    mix-blend-normal
+                    drop-shadow-2xl
+                    border-4
+                    bg-slate-900
+                    border-slate-200
+                    hover:scale-110
+                    hover:cursor-pointer
+                    hover:mix-blend-multiply
+                    transition duration-200"
             />
+            <div
+              id="tooltip-default"
+              role="tooltip"
+              className="absolute z-10 -bottom-5 opacity-0 
+                      w-auto ml-auto mr-auto text-center left-[50%] 
+                      -translate-x-[50%] block px-3 py-2 
+                      text-sm font-medium text-white transition-opacity 
+                      duration-300 delay-200 bg-slate-800 rounded-lg shadow-sm
+                      group-hover:opacity-100
+                      "
+            >
+              Tietoa minusta
+            </div>
           </a>
-        </div>
-      </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[700px]">
+          <DialogHeader>
+            <DialogTitle>Tietoa minusta</DialogTitle>
+          </DialogHeader>
+          <div className="p-10 text-xl font-semibold text-slate-700">
+            <p>
+              Moi, olen Joel Huovinen, tuleva tekniikan opiskelija Tampereen
+              yliopistossa. Vapaa-ajalla teen yleensä jotain luovaa, kuten
+              soitan jazzia pianolla tai suunnittelen graafisia ulkoasuja. Tämä
+              projekti sai alkunsa, kun kerran sain idean tehdä julkiset
+              nettisivut eräälle henkilölle, mutta minulla ei ollut riittävästi
+              taitoa niiden tekemiseen.
+            </p>
+            <br />
+            <p>
+              Niinpä tämä sivusto toimii loistavana alustana kaikenlaisia
+              harjoituksia varten, jotta oppisin luomaan parempia nettisivuja.
+              Nyt olen jo oppinut react-applikaatioiden perusteet ja Tailwind
+              CSS laajennuksen käytön ulkoasujen luomiseksi!
+            </p>
+            <br />
+            <p>
+              Seuraavaksi haluaisin tehdä sivuista interaktiivisia ja toimivia
+              kaikilla laitteilla.
+            </p>
+          </div>
+        </DialogContent>
+      </Dialog>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <p className="  py-10 text-xl text-slate-700 font-sans font-semibold">
+        Joel Huovinen
+      </p>
+      <Badge variant="outline" className="-mt-5">
+        Opiskelija
+      </Badge>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <blockquote className="text-xl italic text-slate-700 max-w-2xl">
+        <svg
+          aria-hidden="true"
+          className="w-10 h-10 text-slate-300"
+          viewBox="0 0 24 27"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <path
+            d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+            fill="currentColor"
+          />
+        </svg>
+        <p>
+          "Joel on innokas luovan taiteen harrastaja, joka ei pelkää kokeilla
+          uusia asioita. Yleensä hänet löytää pianon äärestä, mutta välillä
+          häneen voi törmätä, kun hän on suunnittelemassa graafista ulkoasua tai
+          koodaamassa nettisivuja."
+        </p>
+      </blockquote>
     </main>
-  )
+  );
 }
---
+++ components/GradDecor.js
@@ -0,0 +1,8 @@
+const GradDecor = () => {
+  return (
+  <div>
+  </div>
+  );
+};
+
+export default GradDecor;
